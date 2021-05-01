0ca95c9c933ed32b0b725284f0aa502c068659be
Automedon <42299107+Automedon@users.noreply.github.com>
2021-05-01 16:38:52 +0700

    Updates

---
+++ precache-manifest.50a872e1de166e812351f924ff399fa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b01520d0e4471ee41bac34e046495206",
+    "url": "./index.html"
+  },
+  {
+    "revision": "06f98c730d7c74e4665f",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "9413028210a2a33d07aa",
+    "url": "./static/js/2.77debe0f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.77debe0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06f98c730d7c74e4665f",
+    "url": "./static/js/main.902da771.chunk.js"
+  },
+  {
+    "revision": "86f31f940b867004ca60",
+    "url": "./static/js/runtime-main.ec72d530.js"
+  }
+]);
